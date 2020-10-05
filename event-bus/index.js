211197fddc9ea2082d52ff3e5c6bf8235659cfd4
Ashish Dutta <ashish.dutta@medlife.com>
2020-10-06 00:55:10 +0530

    moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,10 +8,14 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  try {
+    axios.post("http://localhost:4000/events", event);
+    axios.post("http://localhost:4001/events", event);
+    axios.post("http://localhost:4002/events", event);
+    axios.post("http://localhost:4003/events", event);
+  } catch (error) {
+    // throw new Error(error);
+  }
 
   res.send({});
 });
